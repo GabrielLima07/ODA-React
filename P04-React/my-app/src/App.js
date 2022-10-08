ae1862623e5f78d8b84d7080a470ad857b6e71d6
GabrielLima07 <sraa8033@gmail.com>
2022-10-08 15:12:21 -0300

    Upload

---
+++ P04-React/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MeuNome from "./MeuNome";
+
+function App() {
+  return (
+    <div className="App">
+      <MeuNome nome="Vinicius" />
+    </div>
+  );
+}
+
+export default App;
---
+++ P04-React/my-app/src/MeuNome.js
@@ -0,0 +1,9 @@
+function MeuNome(props) {
+    return (
+      <div>
+        <p>Opa {props.nome}, tranquilo?</p>
+      </div>
+    );
+  }
+  
+  export default MeuNome;
